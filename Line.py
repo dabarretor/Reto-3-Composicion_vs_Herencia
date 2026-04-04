9e415e268d25acc4ff09d57d02d560c452eccfb9
Daniel_Barregoat <dabarretor@unal.edu.co>
2026-04-04 17:52:19 -0500

    improvement of point 2

--- Line.py
+++ Line.py
@@ -76,6 +76,17 @@ class Rectangle:
         center_y = (point1.y + point2.y)/2
         return cls(width, height, Point(center_x, center_y))
 
+    @classmethod
+    def method_4(cls, bottom_line: Line, top_line: Line, left_line: Line, right_line: Line):
+        width = bottom_line.compute_length()
+        height = left_line.compute_length()
+            
+            # Calculamos el centro promediando los puntos de las líneas
+        center_x = (bottom_line.start.x + bottom_line.end.x) / 2
+        center_y = (left_line.start.y + left_line.end.y) / 2
+            
+            # Reutilizamos el __init__ original
+        return cls(width, height, Point(center_x, center_y))
     def compute_area(self):
         return self.width * self.height
 
@@ -83,6 +94,7 @@ class Rectangle:
         return 2*(self.width) + 2*(self.height)
 
     def compute_interference_point(self, point: Point):
+
         """ This fuction determinate if a point is inside the rectangle or not. 
         For this, the maximum and minimum values of x and y 
         that a point can have to be inside the rectangle are calculated.
@@ -97,19 +109,81 @@ class Rectangle:
         the rectangle and the function returns True. 
         Otherwise, it returns False.
         """
+
         if ( Max_x >= point.x >= Min_x and Max_y >= point.y >= Min_y):
             return True
         else:
             return False
 
     def compute_interference_line(self, line: Line):
+
         """Use the compute_interference_point function to determine
         if at least one point is inside the rectangle.
         If so, the line segment interferes with the rectangle, 
         and the function returns True. Otherwise, it returns False."""
+
         is_start_inside = self.compute_interference_point(line.start)
         is_end_inside = self.compute_interference_point(line.end)
         if is_start_inside or is_end_inside:
             return True
         else:
             return False
+
+if __name__  == "__main__":
+
+    point1 = Point(0.5, -3.54)
+    point2 = Point(4.5, 0.46)
+    # of the line 138 to 132 is of the class Rectangle, 
+    # using two points as opposite corners (method_3).
+    rectangle = Rectangle.method_3(point1, point2)
+    area = rectangle.compute_area()
+    perimeter = rectangle.compute_perimeter()
+    interference = rectangle.compute_interference_point(Point(2, -1))
+    interference_line = rectangle.compute_interference_line(Line(Point(0, 0), Point(5, 0)))
+
+    print("RECTANGLE DATA:")
+    print(f"Width: {rectangle.width} and Height: \
+          {rectangle.height}") # Output: Width: 4.0 and Height: 4.0
+    print(f"Center Point: ({rectangle.center_point.x},\
+          {rectangle.center_point.y})") # Output: Center Point: (2.5, -1.54)
+    print(f"Area: {area}") # Output: Area: 16.0
+    print(f"Perimeter: {perimeter}") # Output: Perimeter: 16.0
+    print(f"Interference: {interference}") # Output: Interference: True
+    print(f"Interference Line: {interference_line}") # Output: Interference Line: False
+
+    print("---  test of point 2: New method with four lines (method_4) ---")
+    p1 = Point(0, 0)
+    p2 = Point(4, 0)
+    p3 = Point(0, 3)
+    p4 = Point(4, 3)
+
+    line_bottom = Line(p1, p2)
+    line_top = Line(p3, p4)
+    line_left = Line(p1, p3)
+    line_right = Line(p2, p4)
+
+    # A new rectangle is created using 4 lines
+    rect_from_lines = Rectangle.method_4(line_bottom, line_top, line_left, line_right)
+    print(f"Area: {rect_from_lines.compute_area()}") # Output: Area: 12.0
+    print(f"Perimeter: {rect_from_lines.compute_perimeter()}") # Output: Perimeter: 14.0
+    print(f"\n{'-'*30}")
+
+    # of the line 134 to 143 is of the class Line
+    line = Line(Point(1, 2), Point(4, 6))
+    length = line.compute_length()
+    slope = line.compute_slope()
+    horizontal_cross = line.compute_horizontal_cross()
+    vertical_cross = line.compute_vertical_cross()
+
+    print("\nLINES DATA: ")
+    print(f"length: {line.compute_length()}") # Output: length: 5.0
+    print(f"slope: {line.compute_slope()}") # Output: slope: 53.13
+    # Output: horizontal cross: False
+    print(f"horizontal cross: {line.compute_horizontal_cross()}")
+    # Output: vertical cross: False 
+    print(f"vertical cross: {line.compute_vertical_cross()}") 
+    
+
+    
+
+
--- restaurante.py
+++ restaurante.py
@@ -1,26 +1,28 @@
-"""This program allows us to create a restaurant with a menu of 10 items (3 drinks, 3 main dishes, 2 desserts, and 2 additional dishes). 
+"""This program allows us to create a restaurant with a menu of 10 
+(3 drinks, 3 main dishes, 2 desserts, and 2 additional dishes). 
 We take customer orders and apply discounts depending on the combo they order.
 The program demonstrates the use of classes, composition, and inheritance in Python.
 """
+
 class Restaurant:
     def __init__(self, name: str, size: str):
         self.name = name
         self.size = size
         # Composition: the restaurant is created with its own menu
-        self.menu = []
+        self.menu: list["MenuItem"] = []
         self.create_menu() # We call a function to create the 10 items
 
     def create_menu(self):
         # There are your 10 minimum items
         # 3 Drinks (type_drink, size, name, price)
         self.menu.append(Drinks("Gaseosa", "Mediana", "Coca-Cola", 2.50))
-        self.menu.append(Drinks("Juice", "Big", "Natural lemonade", 3.00))
-        self.menu.append(Drinks("Beer", "Small", "Corona", 4.00))
+        self.menu.append(Drinks("Juice", "Big", "Natural lemonade", 3.0))
+        self.menu.append(Drinks("Beer", "Small", "Corona", 4.0))
         
         # 3 Main Dishes (dish_type, dish_size, name, price)
-        self.menu.append(PrincipalDishes("Fast food", "Double", "Hamburguer", 12.00))
-        self.menu.append(PrincipalDishes("Spaghetti", "Personal", "Spaghetti", 10.00))
-        self.menu.append(PrincipalDishes("Meat", "Family", "Chopped meat", 25.00))
+        self.menu.append(PrincipalDishes("Fast food", "Double", "Hamburguer", 12.0))
+        self.menu.append(PrincipalDishes("Spaghetti", "Personal", "Spaghetti", 10.0))
+        self.menu.append(PrincipalDishes("Meat", "Family", "Chopped meat", 25.0))
         
         # 2 Desserts (dessert_type, portion_size, name, price)
         self.menu.append(Desserts("Cold", "1 scoop", "Ice Cream", 3.50))
@@ -35,20 +37,28 @@ class MenuItem:
         self.name = name
         self.price = price
 
+    def __str__(self) -> str:
+        return f"{self.name} (${self.price:.2f})"
+
 class Order:
     def __init__(self, name: str):  
-        # allows us to identify the order, for example, if it's for a specific table or a takeout order. 
+        # allows us to identify the order, for example,
+        # if it's for a specific table or a takeout order. 
         self.name = name
-        self.items = []
-    # The add_item method allows us to add items to the order, which will be stored in the items list.
+        self.items: list["MenuItem"] = []
+
+    # The add_item method allows us to add items to the order,
+    # which will be stored in the items list.
     def add_item(self, item: MenuItem):
         self.items.append(item)
+
     # The total_price method calculates the total price of all items in the order.
     def total_price(self):
         total = 0.0
         for item in self.items:
                 total += item.price
         return total
+    
     """ 
     In apply_discount allows identify the orders for determine 
     if gets discount of 15% or 10%.
@@ -57,12 +67,16 @@ class Order:
     Receive a 15% discount when you place all orders from all subclasses.
     """ 
     def apply_discount(self):
-        # allows us to identify if the order includes items from each of the subclasses (Drinks, PrincipalDishes, Desserts, AdditionalDishes) to determine the applicable discount.
+        # allows us to identify if the order includes items from each of the subclasses
+        # (Drinks, PrincipalDishes, Desserts, AdditionalDishes)
+        # to determine the applicable discount.
         asked_drink = False
         asked_principal_dishes = False
         asked_desserts = False
         asked_additional_dishes = False
-        # We iterate through the items in the order and check their type to set the corresponding flags (asked_drink, asked_principal_dishes, etc.) to True if an item of that type is found.
+        # We iterate through the items in the order and check their type to set 
+        # the corresponding flags (asked_drink, asked_principal_dishes, etc.)
+        # to True if an item of that type is found.
         for item in self.items:
             if isinstance(item, Drinks):
                 asked_drink = True
@@ -75,14 +89,15 @@ class Order:
 
         total_price = self.total_price()
          
-        if asked_drink and asked_principal_dishes and asked_desserts and asked_additional_dishes:
+        if asked_drink and asked_principal_dishes and asked_desserts and \
+        asked_additional_dishes:
             discount = total_price * 0.15
-            return total_price - discount
+            return round(total_price - discount, 2)
         elif asked_drink and asked_principal_dishes and asked_additional_dishes:
             discount = total_price * 0.10
-            return total_price - discount
+            return round(total_price - discount, 2)
         else:
-            return total_price
+            return round(total_price, 2)
             
 
 class Drinks(MenuItem):
@@ -111,26 +126,27 @@ class AdditionalDishes(MenuItem):
 
 # test of program functionality
 if __name__ == "__main__":
-    # 1. Abrimos el restaurante (Se crean los 10 platos automáticamente)
-    mi_restaurante = Restaurant("The Corral Python", "Big")
+    # The restaurant opens (with all 10 dishes available)
+    my_restaurant = Restaurant("The Corral Python", "Big")
     
-    # 2. Llega un cliente a la Mesa 1
-    mesa_1 = Order("Table order 1")
+    # The client arrives at table 1
+    table_1 = Order("Table order 1")
     
-    # 3. El cliente pide un Combo 10% (Bebida + Principal + Adicional)
-    print("\n--- Tomando el pedido ---")
-    mesa_1.add_item(mi_restaurante.menu[0]) # Pide la Coca-Cola
-    mesa_1.add_item(mi_restaurante.menu[3]) # Pide la Hamburguesa
-    mesa_1.add_item(mi_restaurante.menu[8]) # Pide las Papas Fritas
-    print("-----------------------------------")
-    print(f"Pedido 1{mi_restaurante.menu[0]}")
-    print(f"Pedido 2{mi_restaurante.menu[3]}") 
-    print(f"Pedido 3{mi_restaurante.menu[8]}")  
-    # 4. Imprimimos los resultados
-    print("\n--- Factura ---")
-    print("-----------------------------------")
-    print(f"Total sin descuento: ${mesa_1.total_price()}")
-    print(f"Descuento aplicado: ${mesa_1.total_price() - mesa_1.apply_discount()}")
-    print(f"Total a pagar (con descuento aplicado): ${mesa_1.apply_discount()}")
-    print("-----------------------------------")
+    # The client orders a 10% combo (Drink + Main Course + Additional Dish)
+    print("--- Taking the order ---".center(38))
+    table_1.add_item(my_restaurant.menu[0]) # Order Coca-Cola
+    table_1.add_item(my_restaurant.menu[3]) # Order Hamburguer
+    table_1.add_item(my_restaurant.menu[8]) # Order the French fries
+    print("-"*35)
+    print(f"Order 1: {my_restaurant.menu[0]}")
+    print(f"Order 2: {my_restaurant.menu[3]}")
+    print(f"Order 3: {my_restaurant.menu[8]}")
+
+    # The results are printed
+    print("Bill".center(35))
+    print("-"*35)
+    print(f"total without discount: ${round(table_1.total_price(), 2)}") # Output: 18.0
+    print(f"Discount applied: ${round(table_1.total_price() - table_1.apply_discount(), 2)}") # Output: 1.8
+    print(f"Total to pay (with discount applied): ${round(table_1.apply_discount(), 2)}") # Output: 16.2
+    print("-"*35)
     print("¡Thanks for your visit, see you later!")
