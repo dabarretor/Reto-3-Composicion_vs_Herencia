5c96c7ac6ffb7fae31bbe407af850e91da79214a
Daniel_Barregoat <dabarretor@unal.edu.co>
2026-04-17 15:06:07 -0500

    correct minor details

--- Line.py
+++ Line.py
@@ -1,4 +1,5 @@
 import math
+
 class Point:
     def __init__(self, x: float, y: float):
         self.x = x
@@ -11,73 +12,84 @@ class Line:
         self.length = self.compute_length()
         self.slope = self.compute_slope()
 
-    def compute_length(self) -> float: 
-        length = math.sqrt(((self.end.x-self.start.x)**2)+((self.end.y-self.start.y)**2))
+    def compute_length(self) -> float:
+        length = math.sqrt(
+            ((self.end.x - self.start.x) ** 2) + ((self.end.y - self.start.y) ** 2)
+        )
         return length
-            
+
     def compute_slope(self) -> float:
         dy = self.end.y - self.start.y
         dx = self.end.x - self.start.x
         radians = math.atan2(dy, dx)
         angle = math.degrees(radians)
         return angle
-    
+
     def compute_horizontal_cross(self) -> bool:
-        if (self.end.y*self.start.y) <= 0:
+        if (self.end.y * self.start.y) <= 0:
             return True
         else:
             return False
-    
+
     def compute_vertical_cross(self) -> bool:
-        if (self.end.x*self.start.x) <= 0:
+        if (self.end.x * self.start.x) <= 0:
             return True
         else:
             return False
 
 class Rectangle:
     def __init__(self, **kwargs):
-        if 'width' in kwargs and 'height' in kwargs and 'bottom_left_corner' in kwargs:
-            self.width = kwargs['width']
-            self.height = kwargs['height']
-            bottom_left_corner = kwargs['bottom_left_corner']
-    
-            center_x = bottom_left_corner.x + (self.width/2) # The center is sought at x.
-            center_y = bottom_left_corner.y + (self.height/2) # The center is sought in y.
+        if "width" in kwargs and "height" in kwargs and "bottom_left_corner" in kwargs:
+            self.width = kwargs["width"]
+            self.height = kwargs["height"]
+            bottom_left_corner = kwargs["bottom_left_corner"]
+
+            center_x = bottom_left_corner.x + (
+                self.width / 2
+            )  # The center is sought at x.
+            center_y = bottom_left_corner.y + (
+                self.height / 2
+            )  # The center is sought in y.
 
             self.center_point = Point(center_x, center_y)
-        
-        elif 'width' in kwargs and 'height' in kwargs and 'center_point' in kwargs:
-            self.width = kwargs['width']
-            self.height = kwargs['height']
-            self.center_point = kwargs['center_point']
-        
-        elif 'point1' in kwargs and 'point2' in kwargs:
-            self.point1 = kwargs['point1']
-            self.point2 = kwargs['point2']
+
+        elif "width" in kwargs and "height" in kwargs and "center_point" in kwargs:
+            self.width = kwargs["width"]
+            self.height = kwargs["height"]
+            self.center_point = kwargs["center_point"]
+
+        elif "point1" in kwargs and "point2" in kwargs:
+            self.point1 = kwargs["point1"]
+            self.point2 = kwargs["point2"]
             width = abs(self.point2.x - self.point1.x)
             height = abs(self.point2.y - self.point1.y)
-            center_x = (self.point1.x + self.point2.x)/2
-            center_y = (self.point1.y + self.point2.y)/2
+            center_x = (self.point1.x + self.point2.x) / 2
+            center_y = (self.point1.y + self.point2.y) / 2
 
             self.width = width
             self.height = height
             self.center_point = Point(center_x, center_y)
-        
-        elif 'bottom_line' in kwargs and 'top_line' in kwargs and 'left_line' in kwargs and 'right_line' in kwargs:
-            self.bottom_line = kwargs['bottom_line']
-            self.left_line = kwargs['left_line']
+
+        elif (
+            "bottom_line" in kwargs
+            and "top_line" in kwargs
+            and "left_line" in kwargs
+            and "right_line" in kwargs
+        ):
+            self.bottom_line = kwargs["bottom_line"]
+            self.left_line = kwargs["left_line"]
             width = self.bottom_line.compute_length()
             height = self.left_line.compute_length()
-            
+
             # We calculate the center point of the rectangle using the midpoint formula
             center_x = (self.bottom_line.start.x + self.bottom_line.end.x) / 2
             center_y = (self.left_line.start.y + self.left_line.end.y) / 2
-            
+
             # The original __init__ is reused to create the rectangle
             self.width = width
             self.height = height
             self.center_point = Point(center_x, center_y)
-        
+
         min_x = self.center_point.x - (self.width / 2)
         max_x = self.center_point.x + (self.width / 2)
         min_y = self.center_point.y - (self.height / 2)
@@ -92,26 +104,32 @@ class Rectangle:
         self.top_line = Line(p_top_left, p_top_right)
         self.left_line = Line(p_bottom_left, p_top_left)
         self.right_line = Line(p_bottom_right, p_top_right)
-            
-        self.lines = [self.bottom_line, self.top_line, self.left_line, self.right_line]
 
+        self.lines = [self.bottom_line, self.top_line, self.left_line, self.right_line]
 
     def compute_area(self):
         return self.width * self.height
 
     def compute_perimeter(self):
-        return 2*(self.width) + 2*(self.height)
+        return 2 * (self.width) + 2 * (self.height)
 
     def compute_interference_point(self, point: Point):
-
-        """ This fuction determinate if a point is inside the rectangle or not. 
-        For this, the maximum and minimum values of x and y 
+        """This fuction determinate if a point is inside the rectangle or not.
+        For this, the maximum and minimum values of x and y
         that a point can have to be inside the rectangle are calculated.
         """
-        Min_x = self.center_point.x - (self.width/2) # Represents the entire left edge.
-        Max_x = self.center_point.x + (self.width/2) # Represents the entire right edge.
-        Min_y = self.center_point.y - (self.height/2) # Represents the entire bottom edge.
-        Max_y = self.center_point.y + (self.height/2) # Represents the entire top edge.
+        Min_x = self.center_point.x - (
+            self.width / 2
+        )  # Represents the entire left edge.
+        Max_x = self.center_point.x + (
+            self.width / 2
+        )  # Represents the entire right edge.
+        Min_y = self.center_point.y - (
+            self.height / 2
+        )  # Represents the entire bottom edge.
+        Max_y = self.center_point.y + (
+            self.height / 2
+        )  # Represents the entire top edge.
 
         """ If the given point has coordinates x and y that are within 
         the calculated maximum and minimum values, then the point is inside
@@ -119,16 +137,15 @@ class Rectangle:
         Otherwise, it returns False.
         """
 
-        if ( Max_x >= point.x >= Min_x and Max_y >= point.y >= Min_y):
+        if Max_x >= point.x >= Min_x and Max_y >= point.y >= Min_y:
             return True
         else:
             return False
 
     def compute_interference_line(self, line: Line):
-
         """Use the compute_interference_point function to determine
         if at least one point is inside the rectangle.
-        If so, the line segment interferes with the rectangle, 
+        If so, the line segment interferes with the rectangle,
         and the function returns True. Otherwise, it returns False."""
 
         is_start_inside = self.compute_interference_point(line.start)
@@ -138,25 +155,30 @@ class Rectangle:
         else:
             return False
 
-if __name__  == "__main__":
-
-    # of the line 138 to 132 is of the class Rectangle, 
+if __name__ == "__main__":
+    # of the line 138 to 132 is of the class Rectangle,
     # using two points as opposite corners (method_3).
-    rectangle = Rectangle(point1 = Point(0.5, -3.54), point2 = Point(4.5, 0.46))
+    rectangle = Rectangle(point1=Point(0.5, -3.54), point2=Point(4.5, 0.46))
     area = rectangle.compute_area()
     perimeter = rectangle.compute_perimeter()
     interference = rectangle.compute_interference_point(Point(2, -1))
-    interference_line = rectangle.compute_interference_line(Line(Point(0, 0), Point(5, 0)))
+    interference_line = rectangle.compute_interference_line(
+        Line(Point(0, 0), Point(5, 0))
+    )
 
     print("RECTANGLE DATA:")
-    print(f"Width: {rectangle.width} and Height: \
-          {rectangle.height}") # Output: Width: 4.0 and Height: 4.0
-    print(f"Center Point: ({rectangle.center_point.x},\
-          {rectangle.center_point.y})") # Output: Center Point: (2.5, -1.54)
-    print(f"Area: {area}") # Output: Area: 16.0
-    print(f"Perimeter: {perimeter}") # Output: Perimeter: 16.0
-    print(f"Interference: {interference}") # Output: Interference: True
-    print(f"Interference Line: {interference_line}") # Output: Interference Line: False
+    print(
+        f"Width: {rectangle.width} and Height: \
+          {rectangle.height}"
+    )  # Output: Width: 4.0 and Height: 4.0
+    print(
+        f"Center Point: ({rectangle.center_point.x},\
+          {rectangle.center_point.y})"
+    )  # Output: Center Point: (2.5, -1.54)
+    print(f"Area: {area}")  # Output: Area: 16.0
+    print(f"Perimeter: {perimeter}")  # Output: Perimeter: 16.0
+    print(f"Interference: {interference}")  # Output: Interference: True
+    print(f"Interference Line: {interference_line}")  # Output: Interference Line: False
 
     print("---  test of point 2: New method with four lines (method_4) ---")
     p1 = Point(0, 0)
@@ -165,10 +187,17 @@ if __name__  == "__main__":
     p4 = Point(4, 3)
 
     # A new rectangle is created using 4 lines
-    rect_from_lines = Rectangle(bottom_line = Line(p1, p2), top_line = Line(p3, p4), left_line = Line(p1, p3), right_line = Line(p2, p4))
-    print(f"Area: {rect_from_lines.compute_area()}") # Output: Area: 12.0
-    print(f"Perimeter: {rect_from_lines.compute_perimeter()}") # Output: Perimeter: 14.0
-    print(f"\n{'-'*30}")
+    rect_from_lines = Rectangle(
+        bottom_line=Line(p1, p2),
+        top_line=Line(p3, p4),
+        left_line=Line(p1, p3),
+        right_line=Line(p2, p4),
+    )
+    print(f"Area: {rect_from_lines.compute_area()}")  # Output: Area: 12.0
+    print(
+        f"Perimeter: {rect_from_lines.compute_perimeter()}"
+    )  # Output: Perimeter: 14.0
+    print(f"\n{'-' * 30}")
 
     # of the line 134 to 143 is of the class Line
     line = Line(Point(1, 2), Point(4, 6))
@@ -178,9 +207,9 @@ if __name__  == "__main__":
     vertical_cross = line.compute_vertical_cross()
 
     print("\nLINES DATA: ")
-    print(f"length: {line.compute_length()}") # Output: length: 5.0
-    print(f"slope: {line.compute_slope()}") # Output: slope: 53.13
+    print(f"length: {line.compute_length()}")  # Output: length: 5.0
+    print(f"slope: {line.compute_slope()}")  # Output: slope: 53.13
     # Output: horizontal cross: False
     print(f"horizontal cross: {line.compute_horizontal_cross()}")
-    # Output: vertical cross: False 
-    print(f"vertical cross: {line.compute_vertical_cross()}") 
+    # Output: vertical cross: False
+    print(f"vertical cross: {line.compute_vertical_cross()}")
--- restaurante.py
+++ restaurante.py
@@ -1,5 +1,5 @@
-"""This program allows us to create a restaurant with a menu of 10 
-(3 drinks, 3 main dishes, 2 desserts, and 2 additional dishes). 
+"""This program allows us to create a restaurant with a menu of 10
+(3 drinks, 3 main dishes, 2 desserts, and 2 additional dishes).
 We take customer orders and apply discounts depending on the combo they order.
 The program demonstrates the use of classes, composition, and inheritance in Python.
 """
@@ -10,7 +10,7 @@ class Restaurant:
         self.size = size
         # Composition: the restaurant is created with its own menu
         self.menu: list["MenuItem"] = []
-        self.create_menu() # We call a function to create the 10 items
+        self.create_menu()  # We call a function to create the 10 items
 
     def create_menu(self):
         # There are your 10 minimum items
@@ -18,22 +18,22 @@ class Restaurant:
         self.menu.append(Drinks("Gaseosa", "Mediana", "Coca-Cola", 2.50))
         self.menu.append(Drinks("Juice", "Big", "Natural lemonade", 3.0))
         self.menu.append(Drinks("Beer", "Small", "Corona", 4.0))
-        
+
         # 3 Main Dishes (dish_type, dish_size, name, price)
         self.menu.append(PrincipalDishes("Fast food", "Double", "Hamburguer", 12.0))
         self.menu.append(PrincipalDishes("Spaghetti", "Personal", "Spaghetti", 10.0))
         self.menu.append(PrincipalDishes("Meat", "Family", "Chopped meat", 25.0))
-        
+
         # 2 Desserts (dessert_type, portion_size, name, price)
         self.menu.append(Desserts("Cold", "1 scoop", "Ice Cream", 3.50))
         self.menu.append(Desserts("Hot", "1 portion", "Brownie", 4.50))
-        
+
         # 2 AdditionalDishes (portion_size, name, price)
         self.menu.append(AdditionalDishes("for share", "French fries", 3.50))
         self.menu.append(AdditionalDishes("Personal", "salad", 2.25))
 
 class MenuItem:
-    def __init__(self, name: str, price: float):    
+    def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
 
@@ -41,9 +41,9 @@ class MenuItem:
         return f"{self.name} (${self.price:.2f})"
 
 class Order:
-    def __init__(self, name: str):  
+    def __init__(self, name: str):
         # allows us to identify the order, for example,
-        # if it's for a specific table or a takeout order. 
+        # if it's for a specific table or a takeout order.
         self.name = name
         self.items: list["MenuItem"] = []
 
@@ -56,16 +56,17 @@ class Order:
     def total_price(self):
         total = 0.0
         for item in self.items:
-                total += item.price
+            total += item.price
         return total
-    
+
     """ 
     In apply_discount allows identify the orders for determine 
     if gets discount of 15% or 10%.
     Receive a 10% discount when you place three orders from the following
     subclasses: drinks, main courses, and side dishes.
     Receive a 15% discount when you place all orders from all subclasses.
-    """ 
+    """
+
     def apply_discount(self):
         # allows us to identify if the order includes items from each of the subclasses
         # (Drinks, PrincipalDishes, Desserts, AdditionalDishes)
@@ -74,7 +75,7 @@ class Order:
         asked_principal_dishes = False
         asked_desserts = False
         asked_additional_dishes = False
-        # We iterate through the items in the order and check their type to set 
+        # We iterate through the items in the order and check their type to set
         # the corresponding flags (asked_drink, asked_principal_dishes, etc.)
         # to True if an item of that type is found.
         for item in self.items:
@@ -88,9 +89,13 @@ class Order:
                 asked_additional_dishes = True
 
         total_price = self.total_price()
-         
-        if asked_drink and asked_principal_dishes and asked_desserts and \
-        asked_additional_dishes:
+
+        if (
+            asked_drink
+            and asked_principal_dishes
+            and asked_desserts
+            and asked_additional_dishes
+        ):
             discount = total_price * 0.15
             return round(total_price - discount, 2)
         elif asked_drink and asked_principal_dishes and asked_additional_dishes:
@@ -98,14 +103,12 @@ class Order:
             return round(total_price - discount, 2)
         else:
             return round(total_price, 2)
-            
 
 class Drinks(MenuItem):
     def __init__(self, type_drink: str, size: str, name: str, price: float):
         super().__init__(name, price)
         self.type = type_drink
         self.size = size
-    
 
 class PrincipalDishes(MenuItem):
     def __init__(self, dish_type: str, dish_size: str, name: str, price: float):
@@ -115,7 +118,7 @@ class PrincipalDishes(MenuItem):
 
 class Desserts(MenuItem):
     def __init__(self, dessert_type: str, portion_size: str, name: str, price: float):
-        super().__init__(name, price)    
+        super().__init__(name, price)
         self.dessert_type = dessert_type
         self.portion_size = portion_size
 
@@ -128,25 +131,29 @@ class AdditionalDishes(MenuItem):
 if __name__ == "__main__":
     # The restaurant opens (with all 10 dishes available)
     my_restaurant = Restaurant("The Corral Python", "Big")
-    
+
     # The client arrives at table 1
     table_1 = Order("Table order 1")
-    
+
     # The client orders a 10% combo (Drink + Main Course + Additional Dish)
     print("--- Taking the order ---".center(38))
-    table_1.add_item(my_restaurant.menu[0]) # Order Coca-Cola
-    table_1.add_item(my_restaurant.menu[3]) # Order Hamburguer
-    table_1.add_item(my_restaurant.menu[8]) # Order the French fries
-    print("-"*35)
+    table_1.add_item(my_restaurant.menu[0])  # Order Coca-Cola
+    table_1.add_item(my_restaurant.menu[3])  # Order Hamburguer
+    table_1.add_item(my_restaurant.menu[8])  # Order the French fries
+    print("-" * 35)
     print(f"Order 1: {my_restaurant.menu[0]}")
     print(f"Order 2: {my_restaurant.menu[3]}")
     print(f"Order 3: {my_restaurant.menu[8]}")
 
     # The results are printed
     print("Bill".center(35))
-    print("-"*35)
-    print(f"total without discount: ${round(table_1.total_price(), 2)}") # Output: 18.0
-    print(f"Discount applied: ${round(table_1.total_price() - table_1.apply_discount(), 2)}") # Output: 1.8
-    print(f"Total to pay (with discount applied): ${round(table_1.apply_discount(), 2)}") # Output: 16.2
-    print("-"*35)
+    print("-" * 35)
+    print(f"total without discount: ${round(table_1.total_price(), 2)}")  # Output: 18.0
+    print(
+        f"Discount applied: ${round(table_1.total_price() - table_1.apply_discount(), 2)}"
+    )  # Output: 1.8
+    print(
+        f"Total to pay (with discount applied): ${round(table_1.apply_discount(), 2)}"
+    )  # Output: 16.2
+    print("-" * 35)
     print("¡Thanks for your visit, see you later!")
